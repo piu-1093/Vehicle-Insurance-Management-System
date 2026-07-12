598291c97816bb36b0e66878adf0876aec8c4c61
Debopriyo Roy <debopriyoroy2010@gmail.com>
2026-07-12 12:42:42 +0530

    feat: on first visit to login page underwriter  should be selected

--- Group_project/components/login/login.js
+++ Group_project/components/login/login.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ---------------------------------------------------------
     // Application State Variables
     // ---------------------------------------------------------
-    let selectedRole = null; // Can be 'Administrator' or 'Underwriter'
+    let selectedRole = 'Underwriter'; // Can be 'Administrator' or 'Underwriter'
     let alertTimeout;
     // ---------------------------------------------------------
     // SVG Paths for Password Visibility Toggle
@@ -52,6 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
         <line x1="1" y1="1" x2="23" y2="23"></line>
     `;
+
 
     // ---------------------------------------------------------
     // Event Handlers: Role Selection
